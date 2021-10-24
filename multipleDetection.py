c42a17006c03cd278a66b56749b840d709fb8702
Gustavo Caetano da Silva <92827979+caetanogu@users.noreply.github.com>
2021-10-23 22:36:37 -0300

    Update multipleDetection.py

--- multipleDetection.py
+++ multipleDetection.py
@@ -1,38 +1,99 @@
+'''
+This program purpose is to find templates on the given image and use it to draw rectangles, circles or marks
+on the object wanted to find. The user must input the name and the extension of the image and the template desired
+plus the threshold that match better and the mode (rectangles, markers or circles).
+'''
+#Libraries imports
 import cv2 as cv
 import numpy as np
 
-#Carrega as imagens e o template requisitado
-img = cv.imread('albion.jpg', cv.IMREAD_UNCHANGED)
-temp = cv.imread('cabbage_a.jpg', cv.IMREAD_UNCHANGED)
+def findObjects(image, template, threshold, mode):
 
-#Carrega a lista de métodos possíveis a serem utilizados
-methods = [cv.TM_CCOEFF, cv.TM_CCOEFF_NORMED, cv.TM_CCORR,
-            cv.TM_CCORR_NORMED, cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]
+    #Load the base image and the template
+    img = cv.imread(image, cv.IMREAD_UNCHANGED)
+    templ = cv.imread(template, cv.IMREAD_UNCHANGED)
 
-#Aplica o filtro na imagem
-result = cv.matchTemplate(img,temp,methods[5])
+    #List all the methods possible
+    methods = [cv.TM_CCOEFF, cv.TM_CCOEFF_NORMED, cv.TM_CCORR, 
+                cv.TM_CCORR_NORMED, cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]
 
-#Determina a confianca da imagem
-threshold = 0.14
+    #Matches the template in the image given
+    result = cv.matchTemplate(img, templ, methods[1])
 
-#Encontra as posições dos objetos desejados
-locations = np.where(result <= threshold)
-locations = list(zip(*locations[::-1]))
-#print(locations)
+    #Set the threshold of the match
+    thr = threshold
 
-if locations:
-    print('Imagem encontrada')
+    #This takes the position where the template match and zip this positions into tuples
+    locations = np.where(result >= thr)
+    locations = list(zip(*locations[::-1]))
 
-    #Loop para encontrar as posições dos retângulos
-    for loc in locations:
-        top_left = loc
-        bottom_right = (top_left[0] + temp.shape[1], top_left[1] + temp.shape[0])
+    if locations:
+        print('Template found')
 
-        #Desenha os retangulos nas posições encontradas
-        cv.rectangle(img, top_left, bottom_right, color=(0, 0, 255), thickness=2, lineType=cv.LINE_4)
-    
-    cv.imshow('Display', img)
-    cv.waitKey()
+        #Takes the width and the height of the template given
+        width = templ.shape[1]
+        height = templ.shape[0]
 
-else:
-    print('Imagem não encontrada')
+        #Creates a list of rectangles with their (x, y, w, h) coordinates
+        rectangles = []
+
+        #This loops around all the rectangles that must be draw
+        for loc in locations:
+            
+            #This create one rectangle with (x, y, w, h) for each loop
+            rect = (int(loc[0]), int(loc[1]), width, height)
+
+            #Then it adds the rectangle twice to always have more than 1 rectangle in the same location to group
+            rectangles.append(rect)
+            rectangles.append(rect)
+        
+        rectList, weights = cv.groupRectangles(rectangles, groupThreshold=1, eps=0.5)
+
+        points = []
+
+        #Loop to draw all the rectangles
+        for (x, y, w, h) in rectList:
+
+            # Determine the center position
+            center_x = x + int(w/2)
+            center_y = y + int(h/2)
+            
+            # Save the points
+            points.append((center_x, center_y))
+
+            if mode == 'Rectangles':
+
+                #Determine the box position
+                top_left = (x, y)
+                bottom_right = (x + w, y + h)
+
+                #Draw the rectangles
+                cv.rectangle(img, top_left, bottom_right, color=(0, 0, 255))
+
+            elif mode == 'Markers':
+                
+                #Draw the center point
+                cv.drawMarker(img, (center_x, center_y), color=(0, 0, 255), markerType=cv.MARKER_CROSS)
+
+            elif mode == 'Circles':
+
+                #Draw the circle
+                cv.circle(img, (center_x, center_y), radius=10, color=(0, 0, 255))
+
+            else:
+                #If the mode is not recognized pass
+                pass
+
+        #show the image with the markers in the detected objects
+        cv.imshow('Display', img)
+        cv.waitKey()
+
+    else:
+        print('Image not found')
+
+#Principal routine
+print('Welcome to the multiple detection program, this is my first project so sorry if you experience some problems along the way')
+image = input("Insert the image and the extension name (ex: Mario.jpg): ")
+template = input('Then, insert the template and the extension name (ex: Marioface.jpg): ')
+mode = input('Then choose the marker mode (Rectangles, Circles or Markers): ')
+findObjects(image, template, 0.715, mode)
